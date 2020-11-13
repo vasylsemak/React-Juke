87e2d46ceabb282625b99354673ec5298a906c31
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-13 16:37:40 -0500

    create /api/albums route

---
+++ server/api/albums.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Album, Artist } = require('../db/index.js');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const albums = await Album.findAll({ include: [{ model: Artist }] });
+    if(albums.length === 0) res.sendStatus(500);
+    else res.status(200).json(albums);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.get('/:albumId', async (req, res, next) => {
+  try {
+    const album = await Album.findOne({
+      where: { id: req.params.albumId },
+      include: [{ model: Artist }]
+    });
+    if(!album.id) res.sendStatus(500);
+    else res.status(200).json(album);
+  } catch (error) {
+    next(error)
+  }
+});
+
+module.exports = router;
