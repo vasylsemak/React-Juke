0568f0af4e8f40dee3001544f83a8a6eccb70851
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-13 17:57:55 -0500

    add: componentDidMount to fatch albumsData from api

--- client/Main.js
+++ client/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Sidebar from './components/Sidebar';
 import AllAlbums from './components/AllAlbums';
 import Player from './components/Player';
@@ -7,30 +8,13 @@ export default class Main extends React.Component {
   constructor() {
     super();
     this.state = {
-      albums: [
-        {
-          "id": 1,
-          "name": "No Dummy",
-          "artworkUrl": "default-album.jpg",
-          "artistId": 1,
-          "artist": {
-            "id": 1,
-            "name": "The Crash Test Dummies"
-          }
-        },
-        {
-          "id": 2,
-          "name": "I React to State",
-          "artworkUrl": "default-album.jpg",
-          "artistId": 1,
-          "artist": {
-            "id": 1,
-            "name": "The Crash Test Dummies"
-          }
-        }
-      ]
+      albums: []
     }
+  }
 
+  async componentDidMount() {
+    const { data } = await axios.get('/api/albums');
+    this.setState(() => ({ albums: data }));
   }
 
   render () {
